a23208727b54e801f945c509bb2ee7fc5e960af8
robermaher <robermaher@gmail.com>
2024-10-13 21:50:25 +0300

    RC car project

---
+++ APP/main.c
@@ -0,0 +1,57 @@
+/*
+ * RCcar.c
+ *
+ * Created: 10/13/2024 9:06:03 PM
+ * Author : Rober Maher
+ */ 
+
+#include <avr/io.h>
+#include "../LIB/BIT_MATH.h"
+#include "../LIB/STD_TYPE.h"
+
+#include "../MCAL/DIO/DIO_interface.h"
+#include "../MCAL/UART/UART_interface.h"
+
+#include "../HAL/DCMOTOR/DCMOTOR_interface.h"
+
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+
+
+int main(void)
+{
+	DIO_voidSetPinDir(DIO_PORTD,DIO_PIN0,INPUT);
+	DIO_voidSetPinDir(DIO_PORTD,DIO_PIN1,OUTPUT);
+	UART_voidInit();
+	
+	
+	while (1)
+	{
+		if (UART_u8RX()== 'F' || UART_u8RX()== 'f')
+		{
+			DCMOTOR_voidCW();
+		}
+		
+		else if (UART_u8RX()== 'B' || UART_u8RX()== 'b')
+		{
+			DCMOTOR_voidACW();
+		}
+		
+		else if (UART_u8RX()== 'R' || UART_u8RX()== 'r')
+		{
+			DCMOTOR_voidCWRight();
+		}
+		
+		else if (UART_u8RX()== 'L' || UART_u8RX()== 'l')
+		{
+			DCMOTOR_voidCWLeft();
+		}
+		
+		else if (UART_u8RX()== 'S' || UART_u8RX()== 's')
+		{
+			DCMOTOR_voidStop();
+		}
+		
+	}
+}
---
+++ Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+APP\main.c
+
+HAL\DCMOTOR\DCMOTOR_prog.c
+
+MCAL\DIO\DIO_prog.c
+
+MCAL\UART\UART_prog.c
+
